38a9976d5d093b37730e8f2fff5b8eb97bec62a0
harnaman-hk <harnaman.hk@gmail.com>
2022-04-19 18:53:34 +0530

    tweak /video endpoint

--- quarkus-rest-client/src/main/java/org/gs/AppResource.java
+++ quarkus-rest-client/src/main/java/org/gs/AppResource.java
@@ -5,12 +5,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/video")
 public class AppResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    public String video() {
+        return "this should be a video";
     }
 }
